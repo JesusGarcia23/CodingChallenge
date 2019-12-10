76d0669a27581ce8bd4b20cdc661fd5d88fe2c6f
= <jacex17@gmail.com>
2019-12-10 11:17:44 -0500

    finish form (without css)

--- src/App.js
+++ src/App.js
@@ -1,32 +1,51 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Form from './Components/Form';
 
 class App extends React.Component {
   constructor(props){
     super(props)
     this.state = {
-      
+      fullName:'',
+      email:'',
+      projectRepo:'',
+      projectUrl: '',
+      bestpracticeOOP: 0,
+      modularDevelopment: 0,
+      fulWorkUnderstanding: 0,
+      testing: 0,
+      databaseKnowledge: 0,
+      debugging: 0,
+      probSolvingSkills: 0,
+      javascript: 0,
+      html: 0,
+      css: 0,
+      workingTeam: 0,
+      selfMotivation: 0,
+      communicationSkills: 0,
+      ownEnergyLvl: 0,
+      intAptitude: 0
     }
   }
+
+  handleSubmit = () => {
+
+  }
+
+  updateInput = (e) => {
+    e.preventDefault();
+    this.setState({
+      [e.currentTarget.name]: e.currentTarget.value
+    })
+
+  }
+
 
   render(){
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Form allInfo={this.state} updateInput={this.updateInput} handleSubmit={this.handleSubmit}/>
     </div>
   );
   }
--- src/Components/Form.js
+++ src/Components/Form.js
@@ -1,10 +1,35 @@
 import React from 'react'
 
-const Form = () => {
+const Form = (props) => {
+    const {fullName, email, projectRepo, projectUrl} = props.allInfo
     return(
+        <div> 
         <form>
-        
-        
+        <input type='text' name='fullName' value={fullName} onChange={e => props.updateInput(e)} placeholder='FULL NAME'></input>
+        <input type='email' name='email' value={email} onChange={e => props.updateInput(e)} placeholder='EMAIL'></input>
+        <input type='text' name='projectRepo' value={projectRepo} onChange={e => props.updateInput(e)} placeholder='PROJECT REPO'></input>
+        <input type='text' name='projectUrl' value={projectUrl} onChange={e => props.updateInput(e)} placeholder='PROJECT URL'></input>
+        <div>
+        <label>USING BEST PRACTICES FOR OOP:</label><input type='number' name='bestpracticeOOP' onChange={e => props.updateInput(e)}></input>
+        <label>MODULAR DEVELOPMENT:</label><input type='number' name='modularDevelopment' onChange={e => props.updateInput(e)}></input>
+        <label>FULL STACK WORKFLOW UNDERSTANDING:</label><input type='number' name='fulWorkUnderstanding' onChange={e => props.updateInput(e)}></input>
+        <label>TESTING:</label><input type='number' name='testing' onChange={e => props.updateInput(e)}></input>
+        <label>DATABASE KNOWLEDGE:</label><input type='number' name='databaseKnowledge' onChange={e => props.updateInput(e)}></input>
+        <label>DEBUGGING:</label><input type='number' name='debugging' onChange={e => props.updateInput(e)}></input>
+        <label>PROBLEM SOLVING SKILLS:</label><input type='number' name='probSolvingSkills' onChange={e => props.updateInput(e)}></input>
+        <label>JAVASCRIPT:</label><input type='number' name='javascript' onChange={e => props.updateInput(e)}></input>
+        <label>HTML:</label><input type='number' name='html' onChange={e => props.updateInput(e)}></input>
+        <label>CSS:</label><input type='number' name='css' onChange={e => props.updateInput(e)}></input>
+        <label>WORKING ON A TEAM:</label><input type='number' name='workingTeam' onChange={e => props.updateInput(e)}></input>
+        <label>SELF MOTIVATION:</label><input type='number' name='selfMotivation' onChange={e => props.updateInput(e)}></input>
+        <label>COMMUNICATIONS SKILLS:</label><input type='number' name='communicationSkills' onChange={e => props.updateInput(e)}></input>
+        <label>YOUR OWN ENERGY LEVEL:</label><input type='number' name='ownEnergyLvl' onChange={e => props.updateInput(e)}></input>
+        <label>INTELLIGENCE / APTITUDE:</label><input type='number' name='intAptitude' onChange={e => props.updateInput(e)}></input>
+        </div>
+        <button>SUBMIT</button>
         </form>
+        </div>
     )
 }
+
+export default Form
